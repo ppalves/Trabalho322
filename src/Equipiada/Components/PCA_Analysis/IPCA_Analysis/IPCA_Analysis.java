448eded6889baa54cf9396265552c334b1e73dd2
Marcos Diaz <marcos.dure.diaz@gmail.com>
2019-06-10 00:10:07 -0300

    arrumando erros

--- src/Equipiada/Components/PCA_Analysis/IPCA_Analysis/IPCA_Analysis.java
+++ src/Equipiada/Components/PCA_Analysis/IPCA_Analysis/IPCA_Analysis.java
@@ -1,5 +1,6 @@
 package Equipiada.Components.PCA_Analysis.IPCA_Analysis;
 import java.io.IOException;
+
 public interface IPCA_Analysis {
 	public void pca ();
 	public void showPlot() throws IOException;
--- src/Equipiada/Components/PCA_Analysis/PCA_Analysis/PCA_Analysis.java
+++ src/Equipiada/Components/PCA_Analysis/PCA_Analysis/PCA_Analysis.java
@@ -1,5 +1,7 @@
 package Equipiada.Components.PCA_Analysis.PCA_Analysis;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.IOException;
 import javax.swing.JFrame;
 import javax.swing.ImageIcon;
@@ -8,16 +10,19 @@ import javax.swing.JLabel;
 import Equipiada.Components.PCA_Analysis.IPCA_Analysis.IPCA_Analysis;
 
 public class PCA_Analysis implements IPCA_Analysis {
+	//variaveis para armazenar os PATHs 
 	private String pythonPATH;
 	private String scriptPATH;
 	private String Dataset;
 	
+	//construtor: recebe o PATHs, respectivamente, de execucao do python3, de execucao do script e do Database
 	public PCA_Analysis(String py, String sc, String d){
 		pythonPATH = py;
 		scriptPATH = sc;
 		Dataset = d;
 	}
 	
+	//realiza a Analise
 	public void pca () {
 		String dataset = Dataset;
 		ProcessBuilder builder = new ProcessBuilder();
@@ -25,21 +30,37 @@ public class PCA_Analysis implements IPCA_Analysis {
 						scriptPATH,
 						dataset);
 		try {
-			builder.start();
-			System.out.println("Grafico criado.");
+			Process p = builder.start();
+			BufferedReader reader =
+	                    new BufferedReader(new InputStreamReader(p.getInputStream()));
+			 String pyOut = reader.readLine();
+			 if (pyOut != null) {
+				 System.out.println("Script executado com sucesso.");
+			 }
+			 else {
+				 System.out.println("Falha ao executar o script. Verifique o DatasetPATH e o scriptP");
+			 }
+			 reader.close();
 		} catch (IOException e) {
 			e.printStackTrace();
-			System.out.println("Nao foi possivel abrir o Dataset.");
+			System.out.println("Erro de I/O.s");
 		}
 	}
-
+	
+	//mostra o grafico salvo pelo script
 	public void showPlot(){
 		JFrame frame = new JFrame();
-		JLabel label = new JLabel(new ImageIcon(Dataset.substring(0, (Dataset.length()-4))+".jpeg"));
+		String imagePATH = Dataset.substring(0, (Dataset.length()-4))+".jpg";
+		System.out.println(imagePATH);
+		JLabel label = new JLabel(new ImageIcon(imagePATH));
 		frame.add(label);
-		frame.setDefaultCloseOperation
-		(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.pack();
 		frame.setVisible(true); 
 	}
+	
+	//sobrescreve o toString para retornar o DatasetPath
+	public String toString() {
+		return Dataset;
+	}	
 }
