947798291f2c53d7f753250067c78e1ff0ce8598
AndreisPurim <andreispurim@gmail.com>
2019-06-09 22:47:44 -0300

    JARs Arrumados (2)
    JARs estavam faltando os Templates

--- src/Equipiada/Components/Dialogue/IDialogueFactory/IDialogueFactory.java
+++ src/Equipiada/Components/Dialogue/IDialogueFactory/IDialogueFactory.java
@@ -1,7 +1,6 @@
 package Equipiada.Components.Dialogue.IDialogueFactory;
 
 import Equipiada.Components.Dialogue.Dialogue.Dialogue;
-import Equipiada.Components.Dialogue.IDialogue.IDialogue;
 
 public interface IDialogueFactory {
     public static Dialogue createDialogue(){
